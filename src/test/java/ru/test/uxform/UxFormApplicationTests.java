c9cd491f61b175a7747e7c6fba7dd3ab8ed6f387
Marat Gareev <m.gareev@platformaofd.ru>
2025-04-08 19:25:46 +0300

    Initial commit

---
+++ src/test/java/ru/test/uxform/UxFormApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test.uxform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UxFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
